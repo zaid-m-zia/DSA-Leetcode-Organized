50e8491802d1882304c96500dde5e9960b7d9f9a
Zaid Zia <zaidzia900@gmail.com>
2026-07-11 02:32:00 +0530

    Time: 18 ms (45.44%), Space: 108 MB (15.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+// Solution Using HashSet
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set <Integer> set= new HashSet<>();
+
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            else{
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
