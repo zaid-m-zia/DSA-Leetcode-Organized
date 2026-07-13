10ac492f3874b1a3fc2531ad2ad211ae612f21c1
Zaid Zia <zaidzia900@gmail.com>
2026-07-14 02:11:48 +0530

    Time: 0 ms (100%), Space: 45 MB (12.3%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //implementing binary search lessgooo...
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (target < nums[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
