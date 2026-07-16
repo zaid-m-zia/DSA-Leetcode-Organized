25fdeaee935773396df051f79aca57ac9d424cfe
Zaid Zia <zaidzia900@gmail.com>
2026-07-16 23:45:37 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] != 9) {
-                digits[i] += 1;
+        for(int i = digits.length-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
-        }   
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
+            else{
+                digits[i]=0;
+            }
+        }
+        //if we are reaching here it means that all the digits were 9 (edge case)
+        digits = new int [digits.length+1];
+        digits[0]=1;
         return digits;
     }
 }
