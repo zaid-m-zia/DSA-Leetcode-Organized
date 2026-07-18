8741834615dcaf4136dd9e2aff07459f64d41baa
Zaid Zia <zaidzia900@gmail.com>
2026-07-18 17:16:46 +0530

    Time: 3 ms (22.76%), Space: 44 MB (29.31%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+              //nums1[m+n] and nums2[n]
+        //both the arrays are sorted in ascending order
+        // need to merge the arrays and the final answer should be in nums1
+        //and final nums1 should also be in ascending order.
+
+        for(int i = 0; i<nums2.length;i++){
+
+            nums1[m+i]=nums2[i];
+        }
+        //now our arrays have been merged
+        for(int pass = 0 ; pass<nums1.length-1 ; pass++){
+           for(int i = 0 ; i<nums1.length-1 ; i++){
+                if(nums1[i]>nums1[i+1]){
+                    int temp = nums1[i];
+                    nums1[i] = nums1[i+1];
+                    nums1[i+1] = temp;
+                }
+            } 
+        }
+        
+
+
+    }
+}
